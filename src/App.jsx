0bc6f5b7a969590aab18230eb72b830c2d7074db
Ibo Ndure <orfanos@hotmail.de>
2024-10-09 15:01:39 +0200

    addcolorsnew

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { initialColors } from "./lib/colors";
+import { useState } from "react";
+import ColorForm from "./Components/ColorForm";
 import Color from "./Components/Color/Color";
+import { initialColors } from "./lib/colors";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  const addColor = (newColor) => {
+    setColors([newColor, ...colors]); // Add new color to the top
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
+      <ColorForm addColor={addColor} />
       <div className="color-grid">
-        {initialColors.map((color) => (
+        {colors.map((color) => (
           <Color key={color.id} color={color} />
         ))}
       </div>
